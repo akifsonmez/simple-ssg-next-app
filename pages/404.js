9d9374f05b64039790a83fe564d2d80d01b7d87c
akifsonmez <akifsonmez@outlook.com>
2022-07-10 14:38:23 +0300

    Add redirecting example

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function NotFound() {
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => router.push("/"), 3000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div className="not-found">
-      <h1>Oooooppppss</h1>
-      <h2>Page not found</h2>
+      <h1>Redirecting to Homepage...</h1>
+      <h2>Oooooppppss</h2>
+      <h3>Page not found</h3>
       <p>
         Return{" "}
         <Link href="/">
