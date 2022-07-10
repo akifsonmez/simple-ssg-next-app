21a74869941c32c27142c35e6931e886dcbdabd9
akifsonmez <akifsonmez@outlook.com>
2022-07-10 11:40:23 +0300

    Create layout

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
@@ -19,7 +18,6 @@ export default function Home() {
         ullamcorper, malesuada augue ut, pharetra mauris. Ut sollicitudin lorem
         et leo fermentum scelerisque.
       </p>
-      <Footer />
     </div>
   );
 }
